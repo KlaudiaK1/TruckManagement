0094550d589e07285838bde9303ee163faf55b9a
KlaudiaK1998 <klaudiaklepacka1998@gmail.com>
2020-05-25 11:25:11 +0200

    Add comments

--- src/TruckManagement.java
+++ src/TruckManagement.java
@@ -8,6 +8,7 @@ public class TruckManagement {
 
     public static void main(String[] args) {
         while(true){
+            //go further with one unit of time
             try{
                 step();
             }
@@ -16,11 +17,17 @@ public class TruckManagement {
                 System.out.println(e);
                 break;
             }
+            //print actual status of queue
             status();
+
+            //decrement waiting time for each truck in each queueu
             for(Truck t : queue1){
                 t.waitingTime --;
             }
             for(Truck t : queue2){
+                t.waitingTime --;
+            }
+            for(Truck t : mainQueue){
                 t.waitingTime --;
             }
         }
@@ -28,6 +35,7 @@ public class TruckManagement {
     }
     public static void arrive(int weight){
         Truck truck = new Truck(weight);
+        //set proper waiting time for an arriving truck
         if(mainQueue.isEmpty() && !queue1.isEmpty() && !queue2.isEmpty()){
             if(queue1.get(queue1.size() - 1).waitingTime < queue2.get(queue2.size() - 1).waitingTime){
                 truck.waitingTime = queue1.get(queue1.size() - 1).waitingTime + truck.getWeight();
@@ -42,10 +50,13 @@ public class TruckManagement {
         else{
             truck.waitingTime = mainQueue.get(mainQueue.size() - 1).waitingTime + truck.getWeight();
         }
+        //add truck to main queue
         mainQueue.add(truck);
     }
 
     public static int getTimeById(int truckId){
+        //check if there is a truck with the entered id in queues
+        //if yes, return waiting time for the truck
         for(Truck truck : queue1){
             if(truck.getTruckId() == truckId) {
                 return truck.waitingTime;
@@ -119,70 +130,65 @@ public class TruckManagement {
         catch (Exception e){
             throw e;
         }
-
+        //if both queues are empty add new truck to first queue, set proper waiting time and remove it from main queue
         if(queue1.isEmpty() && queue2.isEmpty()){
             queue1.add(mainQueue.get(0));
             Truck addedTruck = queue1.get(0);
             addedTruck.waitingTime = addedTruck.getWeight();
             mainQueue.remove(0);
         }
-        else if (queue1.isEmpty() && !queue2.isEmpty()){
-            queue1.add(mainQueue.get(0));
-            Truck addedTruck = queue1.get(0);
-            addedTruck.waitingTime = addedTruck.getWeight();
-            mainQueue.remove(0);
-            if(queue1.get(0).waitingTime == 0){
-                queue1.remove(0);
-            }
-            if(queue2.get(0).waitingTime == 0){
-                queue2.remove(0);
-            }
-        }
-        else if(!queue1.isEmpty() && queue2.isEmpty()){
-            queue2.add(mainQueue.get(0));
-            Truck addedTruck = queue2.get(0);
-            addedTruck.waitingTime = addedTruck.getWeight();
-            mainQueue.remove(0);
-            if(queue1.get(0).waitingTime == 0){
-                queue1.remove(0);
-            }
-            if(queue2.get(0).waitingTime == 0){
-                queue2.remove(0);
-            }
-        }
         else{
-            if(queue1.size() < 5 && queue2.size() < 5){
-                if(queue1.get(queue1.size() - 1).waitingTime > queue2.get(queue2.size() - 1).waitingTime){
-                    queue2.add(mainQueue.get(0));
-                    Truck addedTruck = queue2.get(queue2.size() - 1);
-                    addedTruck.waitingTime = queue2.get(queue2.size() - 2).waitingTime + addedTruck.getWeight();
-                }
+            //if only one queue is empty, add new truck to the second one
+            if (queue1.isEmpty() && !queue2.isEmpty()){
+                queue1.add(mainQueue.get(0));
+                Truck addedTruck = queue1.get(0);
+                addedTruck.waitingTime = addedTruck.getWeight();
 
-                else{
+            }
+            else if(!queue1.isEmpty() && queue2.isEmpty()){
+                queue2.add(mainQueue.get(0));
+                Truck addedTruck = queue2.get(0);
+                addedTruck.waitingTime = addedTruck.getWeight();
+
+            }
+            else{
+                //if both queues aren't empty and aren't full, add new truck to the queue with shorter waiting time
+                if(queue1.size() < 5 && queue2.size() < 5){
+                    if(queue1.get(queue1.size() - 1).waitingTime > queue2.get(queue2.size() - 1).waitingTime){
+                        queue2.add(mainQueue.get(0));
+                        Truck addedTruck = queue2.get(queue2.size() - 1);
+                        addedTruck.waitingTime = queue2.get(queue2.size() - 2).waitingTime + addedTruck.getWeight();
+                    }
+
+                    else{
+                        queue1.add(mainQueue.get(0));
+                        Truck addedTruck = queue1.get(queue1.size() - 1);
+                        addedTruck.waitingTime = queue1.get(queue1.size() - 2).waitingTime + addedTruck.getWeight();
+                    }
+
+                }
+                //if one of the queue is full, but second one isn't full, add new truck to the second one
+                else if(queue1.size() < 5 && queue2.size() == 5){
                     queue1.add(mainQueue.get(0));
                     Truck addedTruck = queue1.get(queue1.size() - 1);
                     addedTruck.waitingTime = queue1.get(queue1.size() - 2).waitingTime + addedTruck.getWeight();
                 }
-
+                else if(queue1.size() == 5 && queue2.size() < 5){
+                    queue2.add(mainQueue.get(0));
+                    Truck addedTruck = queue2.get(queue2.size() - 1);
+                    addedTruck.waitingTime = queue2.get(queue2.size() - 2).waitingTime + addedTruck.getWeight();
+                }
             }
-            else if(queue1.size() < 5 && queue2.size() == 5){
-                queue1.add(mainQueue.get(0));
-                Truck addedTruck = queue1.get(queue1.size() - 1);
-                addedTruck.waitingTime = queue1.get(queue1.size() - 2).waitingTime + addedTruck.getWeight();
-            }
-            else if(queue1.size() == 5 && queue2.size() < 5){
-                queue2.add(mainQueue.get(0));
-                Truck addedTruck = queue2.get(queue2.size() - 1);
-                addedTruck.waitingTime = queue2.get(queue2.size() - 2).waitingTime + addedTruck.getWeight();
-            }
+            //if waiting time for a first truck is 0, remove it
             if(queue1.get(0).waitingTime == 0){
                 queue1.remove(0);
             }
             if(queue2.get(0).waitingTime == 0){
                 queue2.remove(0);
             }
+            //the first truck from main queue has been added to first or second queue
+            //remove the truck from main queue
             mainQueue.remove(0);
-
         }
 
     }
